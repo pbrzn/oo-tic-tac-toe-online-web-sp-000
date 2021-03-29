a935d7d5443a728a0852d71c6ba73ef4170211ae
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-29 03:15:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,11 +25,10 @@ class TicTacToe
   end
   
   def input_to_index(input)
-    @index=input.to_i - 1
+    index=input.to_i - 1
   end
   
   def move(index, token="X")
-    #token=current_player
     @board[index]=token
   end
   
